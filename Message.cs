da7f5be746e6dd5757d52cf9eb5b306729aeab73
prokelly <kellyrobot247@gmail.com>
2023-01-15 20:45:00 +0100

    Add project files.

---
+++ Message.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CustomMessage
+{
+    public class Message
+    {
+        public static void Error(string message)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        public static void Success(string message)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        public static void Warning(string message)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        public static void Alert(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
